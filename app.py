38fc46b0f20edb1a126c89e379c3e9536559e621
nalkuq <seangleason52@gmail.com>
2022-12-13 08:46:33 -0500

    added requirements.txt

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
