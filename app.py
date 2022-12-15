12de0abcb079c20ed52c8e6143eab681e4910868
nalkuq <seangleason52@gmail.com>
2022-12-15 11:25:34 -0500

    location API added to app.py

--- app.py
+++ app.py
@@ -2,26 +2,43 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask_bootstrap import Bootstrap
-from datetime import datetime
 import requests
+import os
+import folium as fl
+import json
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+def make_user_map(): 
+    def locate_user():
+        client_location=requests.get('http://ip-api.com/json/')
+        response=json.loads(client_location.text)
+        u_long=(response['lon'])
+        u_lat=(response['lat'])
+        ####check documentation about what comes first long or lat
+        folium_map=fl.Map(location=[41.88, -87.62], zoom_start=13, tiles='cartodbpositron', width='75%', height='75%')
+        folium_map.save('index.html')
+    locate_user()
+
+make_user_map()
 
 @app.route("/")
+def login():
+    return render_template("base.html")
+
+@app.route("/home")
 def index():
     return render_template("base.html")
 
-
 @app.route("/start")
-def index():
+def start():
     return render_template("base.html")
 
 
 @app.route("/select")
-def index():
+def select():
     return render_template("base.html")
 
 
