414d9690ec195d04dac553ffd0a13777bec83d66
nalkuq <seangleason52@gmail.com>
2022-12-19 09:05:35 -0500

    updated index.html and main.py

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 from flask import Flask, render_template, request
+import requests
+import json
 
 app = Flask(__name__)
 
 
-def make_user_map():
-    def locate_user():
-        try:
-            client_location = requests.get("http://ip-api.com/json/")
-            response = json.loads(client_location.text)
-            u_long = response["lon"]
-            u_lat = response["lat"]
-        except:
-            pass
-
-    locate_user()
-
-
-make_user_map()
-
+#Need to rewrite so it passes longitude and latitude outside function
+def location_API():
+    try:
+        client_location = requests.get("http://ip-api.com/json/")
+        response = json.loads(client_location.text)
+        longitude = response["lon"]
+        latitude = response["lat"]
+    except:
+        longitude=-161.902701
+        latitude=59.757774
+        pass
 
 @app.route("/")
 def root():
-    return render_template("index.html")
-
+    try:
+        client_location = requests.get("http://ip-api.com/json/")
+        response = json.loads(client_location.text)
+        longitude = response["lon"]
+        latitude = response["lat"]
+    except:
+        longitude=-161.902701
+        latitude=59.757774
+        pass
+    return render_template("index.html", longitude=longitude, latitude=latitude)
 
 if __name__ == "__main__":
     app.run(host="localhost", debug=True)
