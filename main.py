3965c61953d21ca2b8eee9a62161cc2fd93ab648
nalkuq <seangleason52@gmail.com>
2022-12-20 08:18:51 -0500

    updated main.py

--- main.py
+++ main.py
@@ -4,21 +4,7 @@ import json
 
 app = Flask(__name__)
 
-
-# Need to rewrite so it passes longitude and latitude outside function
-def location_API():
-    try:
-        client_location = requests.get("http://ip-api.com/json/")
-        response = json.loads(client_location.text)
-        longitude = response["lon"]
-        latitude = response["lat"]
-    except:
-        longitude = -161.902701
-        latitude = 59.757774
-        pass
-
-
-@app.route("/")
+@app.route("/map", methods=['POST', 'GET'])
 def root():
     try:
         client_location = requests.get("http://ip-api.com/json/")
@@ -29,6 +15,9 @@ def root():
         longitude = -161.902701
         latitude = 59.757774
         pass
+    if request.method=="POST":
+        data=request.get_data()
+        print(data)
     return render_template("index.html", longitude=longitude, latitude=latitude)
 
 
